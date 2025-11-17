132a1c9a08e794a3e5e64300bbb714518fa35aae
pzuczek <pawelzuczek12@gmail.com>
2025-11-17 20:39:20 +0100

    klasu danych + ogólny opis algorytmu

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #TUTAJ ODPALI SIĘ ALGORYTM PO IMPORCIE FUNKCJI I DANYCH
+import numpy as np
+
+
+from BO_2.src import data_matrices as dm
+
+f = dm.floor(np.array([[0,1],[1,0]]), np.array([[1,0],[0,1]]), np.array([[1,1],[1,1]]))
+
+print(f.wall[0,1])
+
--- src/functions.py
+++ src/functions.py
@@ -1,1 +1,3 @@
 #FUNKCJE 
+
+

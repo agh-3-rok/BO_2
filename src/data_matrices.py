4fbbe1c0c434a8f2c8ce5a187a27057887d658c1
pzuczek <pawelzuczek12@gmail.com>
2025-12-01 22:26:27 +0100

    szkielety do struktur danych i funkcji do liczenia celu

--- src/data_matrices.py
+++ src/data_matrices.py
@@ -1,32 +1,34 @@
-
 #tutaj wstawiam kilka danych 
 
 import numpy as np
 from typing import List
 
 
-
-class floor:
+class Floor:
     """
     obiekt przechowujący topografie daneg piętra
     zawiera:
-    wall_matrix: macierz ścian (zawiera współrczynniki tłumienia potrzebne do liczenie zasięgu)
+
+    wall_matrix: macierz floatów
+    macierz ścian (zawiera współrczynniki tłumienia potrzebne do liczenie zasięgu)
     teraz warto zawuażyć że sam krztałt budynku jak i jego pięter może byc dowolny 
     jego krztałt będzie odzwierciedlnoy w przechowwyanych macierzach
     można sobie wyobrazić że macierz ścian stanowi "pokrycie" piętra  
 
-    router_matrix: macierz routerów (zawiera dozwolone współrzędne do stawiania routerów na danym piętrze)
-    macierz binarna 0/1 gdzie 1 oznacza że można postawić routera
+    router_matrix: macierz bool
+    macierz routerów, gdzie 1 oznacza że w tym miejscu można postawić router 
 
-    cover_matrix : macierz zachowująca gdzie chcemy mieć zasięg 
-    tam gdzie nie ma bydunku / lub nie chcemy mieć zasięgu dac inf ;pp   
+    cover_matrix : macierz int 
+    zachowująca gdzie chcemy mieć zasięg 
+    tam gdzie nie ma bydunku / lub nie chcemy mieć zasięgu dac inf ;pp 
 
-    floor_number: numer piętra
+    Floor_number: macierz int
+    numer piętra 
 
-    floor_thickness: grubość podłogi (do liczenia tłumienia między piętrami)
+    Floor_thickness: grubość podłogi (do liczenia tłumienia między piętrami)
     """    
 
-    def __init__(self, wall_matrix: np.ndarray, router_matrix: np.ndarray, cover_matrix: np.ndarray, floor_number: int, floor_thickness: float):
+    def __init__(self, wall_matrix: np.ndarray, router_matrix: np.ndarray, cover_matrix: np.ndarray, Floor_number: int, Floor_thickness: float):
 
         # chcemy mieć pewność że wszystkie macierze mają ten sam rozmiar
         if wall_matrix.shape != router_matrix.shape or wall_matrix.shape != cover_matrix.shape:
@@ -35,59 +37,78 @@ class floor:
         self.wall = wall_matrix
         self.router = router_matrix
         self.cover = cover_matrix
-        self.floor_number = floor_number # to w sumie nie jest potrzebn
-        self.floor_thickness = floor_thickness
+        self.Floor_number = Floor_number # to w sumie nie jest potrzebn
+        self.Floor_thickness = Floor_thickness
 
 def euclidean_distance(point1: np.ndarray, point2: np.ndarray) -> float:
     return np.sqrt(np.sum((point1 - point2) ** 2))  
 
-class building:
+class Point:
+    def __init__(self, x: int, y: int, Floor_number: int):
+        self.x = x
+        self.y = y
+        self.Floor_number = Floor_number
+
+
+class Building:
     # obiekt przechowujący budynek składający się z pięter
     # zawiera listę pięter
 
-    """
-    pomocnicza klasa do definicji punkty w przestrzeni budynku
-    """
-    class point:
-        def __init__(self, x: int, y: int, floor_number: int):
-            self.x = x
-            self.y = y
-            self.floor_number = floor_number
+    # class Point:
+    #     """
+    #     pomocnicza klasa do definicji punkty w przestrzeni budynku
+    #     """
+    #     def __init__(self, x: int, y: int, Floor_number: int):
+    #         self.x = x
+    #         self.y = y
+    #         self.Floor_number = Floor_number
 
-    def __init__(self, floors: list[floor], floor_height: float):
-        self.floor_list = floors
+    def __init__(self, Floors: list[Floor], Floor_heights: float):
+        self.Floor_list = Floors
         self.router_possible = self.__get_possible_router_positions() # od razu buduje liste możliwych pozycji routerów dla łatwiejszego dostępu
         self.points_to_calculate = self.__get_points_to_calculate() # od razu buduje liste punktów do obliczenia zasięgu dla łatwiejszego dostępu
-        self.floor_height = floor_height
+        self.Floor_heights = Floor_heights
         
 
     def __get_possible_router_positions(self) -> np.ndarray:
         # zwraca macierz z możliwymi pozycjami routerów w całym budynku
-        # (x, y, floor_number)
+        # (x, y, Floor_number)
         # ta funkcja będzie ważna w celu zaimplementowanie taboo search!
 
         possible_positions = []
-        for fl in self.floor_list:
+        for fl in self.Floor_list:
             for x in range(fl.router.shape[0]):
                 for y in range(fl.router.shape[1]):
                     if fl.router[x, y] == 1:  # zakładamy, że 1 oznacza dozwoloną pozycję
-                        possible_positions.append((x, y, fl.floor_number))
+                        possible_positions.append((x, y, fl.Floor_number))
 
         return np.array(possible_positions)
 
 
-    def __get_points_to_calculate(self) -> List(point):
+    def __get_points_to_calculate(self) -> list[Point]:
         # zwraca macierz z punktami do obliczenia zasięgu w całym budynku
-        # (x, y, floor_number)
+        # (x, y, Floor_number)
     
         points_to_calculate = []
-        for fl in self.floor_list:
+        for fl in self.Floor_list:
             for x in range(fl.cover.shape[0]):
                 for y in range(fl.cover.shape[1]):
                     if fl.cover[x, y] == 1:  # zakładamy, że 1 oznacza punkt do obliczenia zasięgu
-                        points_to_calculate.append((x, y, fl.floor_number))
+                        points_to_calculate.append(Point(x, y, fl.Floor_number))
 
         return np.array(points_to_calculate)
     
-    def point_distance(self):
+    def point_distance(point1: Point, point2: Point) -> float:
+        # TODO 
+        """
+        metoda licząca dystans międz puntkuami w budynku
+        """
+        pass
+    
+    def get_damping(p1: Point, p2: Point) -> float:
+        # TODO 
+        """
+        metoda licząca tłumienie między piętrami
+        Bresenham's line algorithm może być pomocny
+        """
         pass
--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,30 @@
+import data_matrices as dm
+
 #FUNKCJE 
 
+
+# funkcja celu wyliczana w punkcie
+def goal_function_point(building: dm.Building, point: dm.Point) -> float:
+    """
+    liczymy w decybelach zatem logarytmy zwraca w dB
+    dostaje building i punnkt i liczy wartosc zasiegu w punkcie
+    """ 
+    pass
+
+
+# funkcja celu w całości budynku
+"""
+2 warianty:
+- liczysz wszystko
+- próbujuesz przyspieszyć liczenie z thresholdem - czyli tylko stosunkowo bliskie
+- ewentualnie inny sposób na ułatwienie oblieczeń
+"""
+def goal_function_building(building: dm.Building) -> float:
+    """
+    Dostaje building i liczy wszystko 
+    z uwzglednieniem wag w punktach -> z building.cover 
+
+    """
+
+    pass
 

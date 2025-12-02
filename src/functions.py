a0f26add2cfa1270b05bcd736e438912a922b2f1
Wiktor Majka <wmajka@student.agh.edu.pl>
2025-12-02 11:56:05 +0100

    Zmiana sturktury funkcji, dodanie opisów

--- src/functions.py
+++ src/functions.py
@@ -1,30 +1,56 @@
 import data_matrices as dm
+import numpy as np
+from typing import Tuple, List
 
-#FUNKCJE 
 
-
-# funkcja celu wyliczana w punkcie
-def goal_function_point(building: dm.Building, point: dm.Point) -> float:
+def local_router_range(building: dm.Building, router_point: dm.Point, R_max: int) -> Tuple[np.ndarray, dm.Point]:
     """
-    liczymy w decybelach zatem logarytmy zwraca w dB
-    dostaje building i punnkt i liczy wartosc zasiegu w punkcie
+    Oblicza zasięg od pojedynczego ruter, w jego istotnym otoczeniu. Wartości oblicza się w dB
+    
+    Args:
+        building (dm.Building): budynek
+        router_point (dm.Point): punkt w którym znajduje się ruter
+        R_max (int): threshold dystansu
+    Returns:
+        building_box (np.ndarray) - mała macierz - lokalna mapa zasięgu
+        point (dm.Point) - współrzędne lewego górnego rogu lokalnej macierzy
+    
+    krotka z tych dwóch?
     """ 
-    pass
+    raise NotImplementedError
 
 
-# funkcja celu w całości budynku
+def agregation_func(building: dm.Building, local_ranges: List[Tuple[np.ndarray, dm.Point]]) -> np.ndarray:
+    """
+    Funkcja realizuje wzór na agregację/max sygnału, wykorzystując lokalne zasięgi od ruterów oblicza całą siatkę zasięgów.
+
+    Args:
+        building (dm.Building): budynek
+        local_ranges (list): lista, krotek lokalnych zasięgów i punktów w których zaczyna się siatka
+        
+    Returns:
+        ranges (np.ndarray): siatka zasięgów
+    """
+    raise NotImplementedError
+
+
+def goal_function(building: dm.Building, ranges_matrix: np.ndarray) -> float:
+    """
+    Liczy całościową funkcję celu z uwzglednieniem wag w punktach (building.cover)
+
+    Args:
+        building (dm.Building): budynek
+        ranges_matrix (np.ndarray): siatka zasięgów
+        
+    Returns:
+        goal_func_value (float): wartośc funkcji celu
+    """
+    raise NotImplementedError
+
+
 """
 2 warianty:
 - liczysz wszystko
 - próbujuesz przyspieszyć liczenie z thresholdem - czyli tylko stosunkowo bliskie
 - ewentualnie inny sposób na ułatwienie oblieczeń
 """
-def goal_function_building(building: dm.Building) -> float:
-    """
-    Dostaje building i liczy wszystko 
-    z uwzglednieniem wag w punktach -> z building.cover 
-
-    """
-
-    pass
-

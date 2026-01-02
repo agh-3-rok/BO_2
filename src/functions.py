a62d7b74cc41958a8fb0e46b00ae2a239c740777
Wiktor Majka <wmajka@student.agh.edu.pl>
2026-01-02 22:56:22 +0100

    wyczyszczenie pliku functions

--- src/functions.py
+++ src/functions.py
@@ -1,10 +1,5 @@
 import data_matrices as dm
 import numpy as np
-from typing import Tuple, List
-
-FLOOR_DAMPING_PARAM = 2.0  #przykładowa wartość tłumienia podłogi między piętrami
-TABU_LIST_LENGTH = 10  # przykładowa długość tabu listy
-MAX_ITERATIONS = 100  # przykładowa maksymalna liczba iteracji tabu search
 
 def euclidean_distance(point1: np.ndarray, point2: np.ndarray) -> float:
     return np.sqrt(np.sum((point1 - point2) ** 2))
@@ -23,20 +18,6 @@ def goal_function(building: dm.Building, ranges_matrix: np.ndarray) -> float:
     # NARAZIE POMIJAM ILOSC PIĘTER WYSTACZY DODAC FOR PO PIĘTRACH POTEM
     return np.sum(building.cover * ranges_matrix)
 
-def constructive_change(building: dm.Building, current_solution: List[int], router_usefullnes: List[int]) -> List[int]:
-    #TODO
-    """
-    Funkcja realizująca konstruktywną zmianę w rozmieszczeniu ruterów - liczy użyteczność routerów (ilość obsługiwanych punktów)
-    (#TODO potzebna modifikacja goal function)
-
-    args:
-    building
-
-
-    Returns:
-    całe nowe rozwiązanie czyli (zobacz w tabu_search) listę indeksów rozmieszczenia ruterów
-    """
-    pass
 
     
 
